0538415d1d32b08aed7c86d5be6dfa21b942b610
Marcin Mizgała <maro181114@gmail.com>
2020-04-19 00:21:53 +0200

    Add routing and room model

---
+++ src/app/admin/rooms/rooms.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {DataService} from "../../data.service";
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.css']
+})
+export class RoomsComponent implements OnInit {
+
+  constructor(private dataService : DataService) { }
+
+  ngOnInit(): void {
+    console.log(this.dataService.rooms);
+  }
+
+}
